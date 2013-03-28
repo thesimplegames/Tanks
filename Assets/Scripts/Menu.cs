cceb6a55856f98a4e067c5b2d01a1f8e15368c4a
Dima <Dima@mailforspam.com>
2013-03-28 10:25:02 +0200

    Menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 using System.Collections;
 
 public class Menu : MonoBehaviour {
-
+	
+	public GUIStyle TextStyle, ButtonStyle;
+	bool Settings=false;
+	
 	void OnGUI () {
-		
-		GUI.Button(new Rect(Screen.width/2-50,Screen.height/2-100+10,100,40),"Start game");
-		
-		GUI.Button(new Rect(Screen.width/2-50,Screen.height/2+10,100,40),"Settings");
-		
-		GUI.Button(new Rect(Screen.width/2-50,Screen.height/2+100+10,100,40),"Exit");
-		
-
+		if (!Settings) {
+			GUI.Box (new Rect (Screen.width/2-50, Screen.height/2-Screen.height/4, 100, 40), "Menu", TextStyle);
+			if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/2-Screen.height/16,100,40),"Start game",ButtonStyle)) Application.LoadLevel("tanks");	
+			if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/2+Screen.height/8-Screen.height/16,100,40),"Settings", ButtonStyle)) Settings = true;
+			if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/2+Screen.height/4-Screen.height/16,100,40),"Exit", ButtonStyle)) Application.Quit();
+		}
+		if (Settings) {
+			GUI.Box (new Rect (Screen.width/2-50, Screen.height/2-Screen.height/4, 100, 40), "Settings", TextStyle);
+			if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/2+Screen.height/4-Screen.height/16,100,40),"Back", ButtonStyle)) Settings = false;;
+		}
 	}	
 }
