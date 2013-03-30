41d86cf20a99db78d0c20c77bbfb4fd189330ba3
Ilya <ADG@gm.com>
2013-03-30 11:05:55 +0200

    New_tank_1

---
+++ Assets/Scripts/MapPrefs.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class MapPreferences : MonoBehaviour {
+	
+	public static int heigth;
+	public static int length;
+	public static int enemyCount;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
